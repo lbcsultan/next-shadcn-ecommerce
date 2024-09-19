7bb5af99a1d65ce721bfaecb896af0b10a295f33
Byoungcheon <lbcsultan@gmail.com>
2024-09-19 20:12:54 +0900

    11. create new user

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,7 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
-import { Button } from '@/components/ui/button'
 import { APP_NAME } from '@/lib/constants'
 import Menu from './menu'
 
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,27 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const formatError = (error: any): string => {
+  if (error.name === 'ZodError') {
+    const fieldErrors = Object.keys(error.errors).map((field) => {
+      const errorMessage = error.errors[field].message
+      return `${error.errors[field].path}: ${errorMessage}` // field: errorMessage
+    })
+    return fieldErrors.join('. ')
+  } else if (error.name === 'ValidationError') {
+    const fieldErrors = Object.keys(error.errors).map((field) => {
+      const errorMessage = error.errors[field].message
+      return errorMessage
+    })
+    return fieldErrors.join('. ')
+  } else {
+    return typeof error.message === 'string'
+      ? error.message
+      : JSON.stringify(error.message)
+  }
 }
